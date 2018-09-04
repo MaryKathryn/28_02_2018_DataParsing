0b07fdc2786451b4113959308e4105601b7ff634
MaryKathryn <16MM79@queensu.ca>
2018-09-04 07:52:00 -0400

    ERPStruct - NS4 and Reward
    Takes in a data file (NS4) and a file type ('NS4') and data file (NEV) and returns a 1x64 structure where every cell is an array of ERP data (5 seconds of samples 1 sec before reward given, 4 sec after) for one electrode.

--- GetERPdata.m
+++ GetERPdata.m
@@ -7,7 +7,7 @@
 %Input vector must be made from codes sent to NEV file first 
 %Example that this code works with this
 
-%openNEV('read', "D:\Testing\NSP3_Lennon_joyTrain_20180816030.nev")
+%openNEV('read', 'D:\Testing\NSP3_Lennon_joyTrain_20180816030.nev')
 %rewardTimes = GetRewardTimes_2018_08_15(NEV);
     %this is a functions that returns a vector of the reward times in a 
     %NEV file. 
@@ -16,27 +16,25 @@
 %In this case the fileType is 'NS4' but you could also upload averaged data 
 %if you do that make sure type is 'avShanks'
 
+%ERPdata = GetERPdata(NS4,NEV)
 
 
-
-function  ERPdata = GetERPdata(data,NEV) %data can be NS4/or averaged shanks 
-
-%If you're unsure of the sampling rate run this line  
-%srate = NS4.MetaTags.SamplingFreq; %sampling freq for this session/data
-%file instead of the following line
+function  ERPStruct = GetERPdata(data,fileType,NEV) %data can be NS4/or averaged shanks 
 
 %Initialization
-srate = 10000 %10k samples per second
-pnts = NS4.MetaTags.DataPoints(1,2); %number of data points is in 2nd col
+srate = 10000; %10k samples per second
+ERPStruct = {};
+etrode = 1;
 eventIndex = 1;
+rewardIndex = 1;
 ERPdata = [];
 ERPDataStart = 0;
 ERPDataEnd = 0;   
 wPeakFreq = 10; %peak or center frequency of the wavelet
     
+%% **** IF you are looking for reward ERPs!
+% Get a row vector of times for what ever event you want here!
 
-%% Get a row vector of times for what ever event you want here!
-%reward
 rewardTimes  = GetRewardTimes_2018_08_15(NEV);
 dims = size(rewardTimes);
 nTrials = dims(1,2); %nTrials = number of rewards for this trial
@@ -53,11 +51,13 @@ waveletfft = fft(exp(2*1i*pi*wPeakFreq.*wavetime) .* exp(-wavetime.^2./(2*(5/(2*
 data10hz   = [];
 
 
-if fileType == NS4
+if fileType == 'NS4'
     %this means the function user passed one row of a lot of samples of raw
     %voltage data to this func. 
     
-    voltageRec = NS4.Data{1,2}; %each row of voltage rec will be another electrode
+    dataDims = size(data.Data);%make sure youre getting data from the last (biggest) col in case there were misalignments/paused starts
+    lastCol = dataDims(2);
+    voltageRec = data.Data{1,lastCol}; %each row of voltage rec will be another electrode
     
     %% Error handling
     %make sure index - 10 000 doesnt go below 0 and that index + 40 000 doesn't go
@@ -69,28 +69,37 @@ if fileType == NS4
     sizeRewardTimes = size(rewardTimes);
     maxRewardSamp = sizeRewardTimes(2); %Last reward sample I have to plot 
     
-    %%Start filling the ERP Matricies 
+%% Start filling the ERP Matricies 
+    %ERP data for each electrode will be saved in a giant ERP struct where
+    %each ROW contains numReward rows of ERP data for that electrode
     
-    %%%%stopped here%% edit below 
+while etrode <= 64 %etrode initialized at top as 1
     
-while rewardIndex <= maxRewardSamp &&  ERPDataEnd < samps  &&  ERPDataStart < samps
+    while rewardIndex <= maxRewardSamp &&  ERPDataEnd < samps  &&  ERPDataStart < samps
+
+        ERPDataStart = rewardSamp(rewardIndex);
+        ERPDataStart = ERPDataStart - 10000;
+        ERPDataEnd = rewardSamp(rewardIndex);
+        ERPDataEnd =  ERPDataEnd + 40000;
+
+        if (ERPDataStart < 0)
+            rewardIndex = rewardIndex + 1 
+            %There isn't enough time before this reward to do the analysis so 
+            %just move on to the next reward
+
+        else
+            %Row is just the electrode number, and the cols go from rew-10k to
+            %rew+20k samples 
+            ERPdata(rewardIndex,:) = voltageRec(etrode,ERPDataStart:ERPDataEnd);
+            rewardIndex = rewardIndex + 1; % move on to the next reward
+        end 
+
+    end %while ERPData
     
-    ERPDataStart = rewardSamp(rewardIndex);
-    ERPDataStart = ERPDataStart - 10000;
-    ERPDataEnd = rewardSamp(rewardIndex);
-    ERPDataEnd =  ERPDataEnd + 40000;
-    
-    if (ERPDataStart < 0)
-        rewardIndex = rewardIndex + 1 % move on to the next reward
-        
-    else
-        %Row is just the electrode number, and the cols go from rew-10k to
-        %rew+20k samples 
-        ERPdata(rewardIndex,:) = voltageRec(1,ERPDataStart:ERPDataEnd);
-        rewardIndex = rewardIndex + 1; % move on to the next reward
-    end 
-      
-end %while
+    ERPStruct{etrode} = ERPdata;
+    etrode = etrode + 1;
+
+end %while ERPStruct 
 
 
     
@@ -105,3 +114,9 @@ end
 
 
 end
+
+%If you're unsure of the sampling rate run this line  
+%srate = NS4.MetaTags.SamplingFreq; %sampling freq for this session/data
+%file
+
+%pnts = NS4.MetaTags.DataPoints(1,lastCol); %number of data points is in 2nd col
